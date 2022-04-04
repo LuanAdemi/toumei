197d83aa0ed25289a4af2df9fec8ba38738a413b
Luan Ademi <26838000+LuanAdemi@users.noreply.github.com>
2022-04-04 15:15:35 +0200

    Added the full .idea folder

--- toumei/parameterization/generator.py
+++ toumei/parameterization/generator.py
@@ -5,5 +5,5 @@ class Generator(object):
     def __init__(self):
         super(Generator, self).__init__()
 
-    def getImage(self, z: int = None) -> torch.Tensor:
-        return torch.rand(2)
+    def getImage(self, *args, **kwargs) -> torch.Tensor:
+        return NotImplementedError
