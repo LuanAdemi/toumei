0fa941cc0679cab8dc4f3c3a4d0aaa8897e16e71
Luan Ademi <26838000+LuanAdemi@users.noreply.github.com>
2022-05-01 12:21:15 +0200

    Updated setup.py

--- toumei/setup.py
+++ toumei/setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 setup(
     name='toumei',
     version='0.1',
-    packages=['toumei', 'toumei.probe', 'toumei.objectives', 'toumei.parameterization'],
-    url='',
-    license='',
+    packages=['toumei', 'toumei.probe', 'toumei.objectives', 'toumei.parameterization', 'toumei.misc', 'toumei.models'],
+    url='https://github.com/LuanAdemi/toumei',
+    license='GPL-3.0',
     author='Luan Ademi',
     author_email='luan.ademi@student.kit.edu',
-    description='A library for easy feature visualization'
+    description='A library for easy feature visualization in pytorch'
 )
