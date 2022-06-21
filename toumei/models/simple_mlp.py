70b1b164c4448db5887e61f44543ced486a4d1f3
Luan Ademi <26838000+LuanAdemi@users.noreply.github.com>
2022-06-21 18:20:05 +0200

    Fixed the Bug in MLPGraph introduced by the new SimpleMLP implementation

--- toumei/models/simple_mlp.py
+++ toumei/models/simple_mlp.py
@@ -2,28 +2,21 @@ import torch.nn as nn
 
 
 class SimpleMLP(nn.Module):
-    def __init__(self, inp, out):
+    layers = []
+
+    def __init__(self, *dimensions):
         super(SimpleMLP, self).__init__()
 
-        self.fc1 = nn.Linear(inp, inp)
-        self.relu1 = nn.ReLU()
+        for i, (dimension) in enumerate(dimensions):
+            fc = nn.Linear(dimension, dimensions[i+1])
+            setattr(self, f"fc{i}", fc)
+            self.layers.append(fc)
+            if len(dimensions) == i+2:
+                break
 
-        self.fc2 = nn.Linear(inp, inp//2)
-        self.relu2 = nn.ReLU()
-
-        self.fc3 = nn.Linear(inp//2, inp//4)
-        self.relu3 = nn.ReLU()
-
-        self.fc4 = nn.Linear(inp//4, out * 20)
-        self.relu4 = nn.ReLU()
-
-        self.fc5 = nn.Linear(out * 20, out)
-        self.relu5 = nn.ReLU()
+        self.relu = nn.ReLU()
 
     def forward(self, x):
-        x = self.relu1(self.fc1(x))
-        x = self.relu2(self.fc2(x))
-        x = self.relu3(self.fc3(x))
-        x = self.relu4(self.fc4(x))
-        x = self.relu5(self.fc5(x))
-        return x
+        for layer in self.layers:
+            x = self.relu(layer(x))
+        return
