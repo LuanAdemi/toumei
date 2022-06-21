43e97ade1efcbdedb415909af151bc8796ce6f9e
Davanchama <davanchama@gmail.com>
2022-06-21 18:12:02 +0200

    brought the simple_mlp constructor in a more accessible form

--- tests/test_mlp_modularity.py
+++ tests/test_mlp_modularity.py
@@ -1,11 +1,9 @@
 import sys
 
+sys.path.append("../")
 from toumei.models import SimpleMLP
 from toumei.misc import MLPGraph
-
 from toumei.cnns.objectives.utils import set_seed
-
-sys.path.append("../")
 
 
 def test_mlp_modularity():
