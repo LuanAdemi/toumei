c67f64397b383c9f1e1ea95013c778a090e0f987
Davanchama <davanchama@gmail.com>
2022-07-12 16:54:31 +0200

    Started to implement broadness measurer for one hot encoding. WARNING: not runnable code. this might be my worst commit on a public repo ever. thank you for your attention.

--- experiments/research/eval_model_broadness.py
+++ experiments/research/eval_model_broadness.py
@@ -6,6 +6,7 @@ import torch
 import torch.nn as nn
 import torchvision as tv
 from torchvision.transforms import ToTensor
+from experiments.research.binary_addition.two_numbers_datasets import OneHotEncodingDataset
 
 from toumei.models import SimpleMLP
 
@@ -14,28 +15,27 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-data = tv.datasets.MNIST(root="./data", download=True, train=True, transform=ToTensor())
-dataLoader = torch.utils.data.DataLoader(data, batch_size=1, shuffle=True)
 loss_fc = nn.MSELoss()
-
-network = SimpleMLP(1568, 784, 20, 20, 4, 1).to(device)
-loaded_network = torch.load("models/trained_model.pth", map_location=device)
-network.load_state_dict(loaded_network)
 
 
 # a broadness measurer for patched models
 class BroadnessMeasurer(object):
-    def __init__(self, model):
+    def __init__(self, model, dataLoader):
         self.model = model
+        self.dataLoader = dataLoader
 
     def measure_broadness(self, std_list, num_iters=1):
         losses_measured = []
         for std in std_list:
             if std == 0:
-                #for std 0 always measure on time since there is only one possible outcome
-                losses_measured.append(self.measure_broadness_for_std(std))
+                # for std 0 always measure on time since there is only one possible outcome
+                outcome = self.measure_broadness_for_std(std)
+                losses_measured.append(outcome)
+                print("Losses for std = ", str(std), ": ", outcome)
             else:
-                losses_measured.append(self.measure_broadness_for_std(std, num_iters))
+                outcome = self.measure_broadness_for_std(std, num_iters)
+                losses_measured.append(outcome)
+                print("Losses for std = ", str(std), ": ", outcome)
         sns.heatmap(losses_measured)
         plt.show()
         return losses_measured
@@ -53,16 +53,18 @@ class BroadnessMeasurer(object):
         loss_train = []
         prev_elements = []
         prev_labels = []
-        for h, (element, label) in enumerate(dataLoader):
+        for h, (element, label) in enumerate(self.dataLoader):
             if len(prev_elements) == 0:
                 prev_elements = element
                 prev_labels = label
                 continue
             element = element.to(device)
             label = label.to(device)
-            inp = torch.stack((element, prev_elements), dim=1).view(1, 1568)
+            # TODO: wenn die inp dims probleme machen, view benutzen
+            #  (vermutlich aus self.model die input size rausholen, hab jz aber keine lust mehr das zu machen sry)
+            inp = torch.stack((element, prev_elements), dim=1)
             predicted_result = model(inp)
-            result = torch.FloatTensor([label[0]+prev_labels[0]])
+            result = torch.FloatTensor([label[0] + prev_labels[0]])
             loss = loss_fc(predicted_result, result)
             loss.backward()
             loss_train.append(loss.item())
@@ -87,6 +89,18 @@ class BroadnessMeasurer(object):
         return model
 
 
-measurer = BroadnessMeasurer(network)
-standard_deviations = [x * 0.01 for x in range(6)]
-print(measurer.measure_broadness(standard_deviations, num_iters=5))
+# TODO: max number (und optional len) des dataset ans model anpassen, der dataloader wird dann der get_loss
+#  func übergeben
+#  max number is hier die höchste repräsentierbare zahl im one hot vector, der vector wird also max+1 lang sein
+data = OneHotEncodingDataset(2000, max_number=10)
+beltalowda = torch.utils.data.DataLoader(data, batch_size=16, shuffle=True)
+# TODO: lade die models mit richtigen dimensionen
+network = SimpleMLP(1568, 784, 20, 20, 4, 1).to(device)
+loaded_network = torch.load("models/trained_model.pth", map_location=device)
+network.load_state_dict(loaded_network)
+
+# TODO: erzeuge für jedes model einen measurer
+measurer = BroadnessMeasurer(network, beltalowda)
+# TODO: wenn diese skripte schnell durchlaufen (sollte es bei one hot eig) gerne sample size erhöhen
+standard_deviations = [x * 0.01 for x in range(10)]
+print(measurer.measure_broadness(standard_deviations, num_iters=20))
