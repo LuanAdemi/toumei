8de2dc52335d966e1d6817660e68da062bfd22e6
Davanchama <davanchama@gmail.com>
2022-07-12 13:47:04 +0200

    implemented naive broadness measuring for mnist-add models

--- experiments/research/eval_model_broadness.py
+++ experiments/research/eval_model_broadness.py
@@ -7,18 +7,20 @@ import torch.nn as nn
 import torchvision as tv
 from torchvision.transforms import ToTensor
 
+from toumei.models import SimpleMLP
+
 sys.path.append("../../")
-from experiments.research.patch_model import PatchedModel
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 data = tv.datasets.MNIST(root="./data", download=True, train=True, transform=ToTensor())
-dataLoader = torch.utils.data.DataLoader(data, batch_size=64, shuffle=True)
+dataLoader = torch.utils.data.DataLoader(data, batch_size=1, shuffle=True)
 loss_fc = nn.MSELoss()
 
-network = PatchedModel().to(device)
-network.load_state_dict(torch.load("models/patched_model.pth", map_location=device))
+network = SimpleMLP(1568, 784, 20, 20, 4, 1).to(device)
+loaded_network = torch.load("models/trained_model.pth", map_location=device)
+network.load_state_dict(loaded_network)
 
 
 # a broadness measurer for patched models
@@ -26,7 +28,7 @@ class BroadnessMeasurer(object):
     def __init__(self, model):
         self.model = model
 
-    def measure_broadness(self, std_list, num_iters):
+    def measure_broadness(self, std_list, num_iters=1):
         losses_measured = []
         for std in std_list:
             losses_measured.append(self.measure_broadness_for_std(std, num_iters))
@@ -45,45 +47,42 @@ class BroadnessMeasurer(object):
 
     def get_loss(self, model):
         loss_train = []
-        picture_storage = []
+        prev_elements = []
+        prev_labels = []
         for h, (element, label) in enumerate(dataLoader):
-            # if batch is not complete
-            if element.shape[0] != 64:
-                break
-
+            if len(prev_elements) == 0:
+                prev_elements = element
+                prev_labels = label
+                continue
             element = element.to(device)
             label = label.to(device)
-            # two images needed for input
-            if len(picture_storage) == 0:
-                picture_storage.append((element, label))
-                continue
-            else:
-                (prev_element, prev_label) = picture_storage.pop()
-                result = prev_label + label
-                inp = torch.cat((prev_element.view(-1, 28 * 28), element.view(-1, 28 * 28)), dim=1)
-                predicted_result = model(inp)
-                loss = loss_fc(predicted_result.view(64), result.float())
-                loss.backward()
-                loss_train.append(loss.item())
+            inp = torch.stack((element, prev_elements), dim=1).view(1, 1568)
+            predicted_result = model(inp)
+            result = torch.FloatTensor([label[0]+prev_labels[0]])
+            loss = loss_fc(predicted_result, result)
+            loss.backward()
+            loss_train.append(loss.item())
         return np.mean(loss_train)
 
     # alter every parameter by a std. this should be run multiple times per std
     # the given model should be a patched model
     def alter_params(self, model, standard_deviation=0.01):
-        for named_params in zip(model.patched_m.named_parameters()):
+        for named_params in zip(model.named_parameters()):
             (key, value) = named_params[0]
 
             # it might be interesting to differ weight std and bias std
             if 'weight' in key:
-                for neuron_weights in value:
+                for j in range(len(value)):
+                    neuron_weights = value[j]
                     for i in range(len(neuron_weights)):
                         current_weight = neuron_weights[i]
-                        neuron_weights[i] = np.random.randn() * standard_deviation + neuron_weights[i]
+                        with torch.no_grad():
+                            neuron_weights[i] = np.random.randn() * standard_deviation + neuron_weights[i]
             elif 'bias' in key:
                 pass
         return model
 
 
 measurer = BroadnessMeasurer(network)
-std_list = [x * 0.01 for x in range(10)]
-print(measurer.measure_broadness(std_list, 10))
+standard_deviations = [x * 0.01 for x in range(6)]
+print(measurer.measure_broadness(standard_deviations, num_iters=5))
