917957520f5b13f7710c03c10d6ee43bc7ac7a09
Davanchama <davanchama@gmail.com>
2022-07-13 14:17:39 +0200

    The broadness measurer now can handle different model sizes.

--- experiments/research/eval_model_broadness.py
+++ experiments/research/eval_model_broadness.py
@@ -6,7 +6,7 @@ import torch
 import torch.nn as nn
 import torchvision as tv
 from torchvision.transforms import ToTensor
-from experiments.research.binary_addition.two_numbers_datasets import OneHotEncodingDataset
+from experiments.research.binary_addition.n_numbers_datasets import OneHotEncodingDataset
 
 from toumei.models import SimpleMLP
 
@@ -60,9 +60,10 @@ class BroadnessMeasurer(object):
                 continue
             element = element.to(device)
             label = label.to(device)
-            # TODO: wenn die inp dims probleme machen, view benutzen
-            #  (vermutlich aus self.model die input size rausholen, hab jz aber keine lust mehr das zu machen sry)
             inp = torch.stack((element, prev_elements), dim=1)
+            #TODO: there seems to be a problem here with viewing: loss gets higher with batch rate
+            # -> network gets wrong input?
+            inp = inp.view(self.dataLoader.batch_size, self.get_input_size())
             predicted_result = model(inp)
             result = torch.FloatTensor([label[0] + prev_labels[0]])
             loss = loss_fc(predicted_result, result)
@@ -73,6 +74,8 @@ class BroadnessMeasurer(object):
     # alter every parameter by a std. this should be run multiple times per std
     # the given model should be a patched model
     def alter_params(self, model, standard_deviation=0.01):
+        if standard_deviation == 0:
+            return model
         for named_params in zip(model.named_parameters()):
             (key, value) = named_params[0]
 
@@ -88,19 +91,27 @@ class BroadnessMeasurer(object):
                 pass
         return model
 
+    def get_input_size(self):
+        first_layer = next(self.model.named_parameters())
+        (layer_name, parameters) = first_layer
+        return parameters.size(dim=1)
 
-# TODO: max number (und optional len) des dataset ans model anpassen, der dataloader wird dann der get_loss
-#  func übergeben
-#  max number is hier die höchste repräsentierbare zahl im one hot vector, der vector wird also max+1 lang sein
-data = OneHotEncodingDataset(2000, max_number=10)
-beltalowda = torch.utils.data.DataLoader(data, batch_size=16, shuffle=True)
+
 # TODO: lade die models mit richtigen dimensionen
 network = SimpleMLP(1568, 784, 20, 20, 4, 1).to(device)
 loaded_network = torch.load("models/trained_model.pth", map_location=device)
 network.load_state_dict(loaded_network)
 
+# TODO: max number (und optional len) des dataset ans model anpassen, der dataloader wird dann der get_loss
+#  func übergeben
+#  max number is hier die höchste repräsentierbare zahl im one hot vector, der vector wird also max+1 lang sein
+#data = OneHotEncodingDataset(2000, max_number=10)
+data = tv.datasets.MNIST(root="./data", download=True, train=True, transform=ToTensor())
+beltalowda = torch.utils.data.DataLoader(data, batch_size=16, shuffle=True)
+
 # TODO: erzeuge für jedes model einen measurer
 measurer = BroadnessMeasurer(network, beltalowda)
 # TODO: wenn diese skripte schnell durchlaufen (sollte es bei one hot eig) gerne sample size erhöhen
-standard_deviations = [x * 0.01 for x in range(10)]
+#standard_deviations = [x * 0.01 for x in range(10)]
+standard_deviations = [0]
 print(measurer.measure_broadness(standard_deviations, num_iters=20))
